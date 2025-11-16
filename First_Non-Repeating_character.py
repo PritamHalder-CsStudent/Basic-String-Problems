012c36f9e67f85d74a82cf3af6e72e758033a9c9
PritamHalder-CsStudent <pritamhalder42002@gmail.com>
2025-11-16 20:44:11 +0530

    modification on prob First non repeating character

--- First_Non-Repeating_character.py
+++ First_Non-Repeating_character.py
@@ -6,12 +6,13 @@ def Non_repeat(s):
     res=""
     for ch in s:
         dd[ch]=dd.get(ch,0)+1
-    for ele in dd:
-        if dd[ele] == 1 :
-            return ele
+    for key in dd:
+        if dd[key] == 1 :
+            return key
     return None
         
-
+# In leetcode problem we return answer as index of the non repeating character , 
+# so in place of return ele , use return s.index(key) that will give index of the non repeating character
 
 ans= Non_repeat("swiss")
 print(ans)
