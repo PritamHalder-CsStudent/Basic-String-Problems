0409e5dc054974749f6f9b0fa1b8df563cac249b
PritamHalder-CsStudent <pritamhalder42002@gmail.com>
2025-11-24 20:09:49 +0530

    problem updated

--- String_Propercase.py
+++ String_Propercase.py
@@ -14,3 +14,33 @@ ans=String_Proper(s)
 print(ans)
 
 
+
+# Updated version of the problem:
+
+# Given a string S consisting of only lowercase and uppercase English letters and spaces, 
+# your task is to convert it into title case. In title case, the first letter of each word is 
+# capitalized while the rest are in lowercase, 
+# except for words that are entirely in uppercase (considered as acronyms), which should remain unchanged.
+
+# Sample test case :
+# Input: this is a CODECHEF problem
+# output: This Is A CODECHEF Problem
+
+
+def convert_titleCase(s):
+    str_arr = s.split(" ")
+
+    for i in range(len(str_arr)):
+        # If the entire word is uppercase (acronym), skip formatting
+        if str_arr[i].isupper():
+            continue
+        # Otherwise convert to title-case (capital first letter, rest lowercase)
+        str_arr[i] = str_arr[i].capitalize()
+
+    return " ".join(str_arr)
+
+
+t = int(input())
+for _ in range(t):
+    s = input()
+    print(convert_titleCase(s))
